d675e1a9e8111a8900e1d07fe8457e6fd537dc35
Enkindu Weles <enkinduweles@gmail.com>
2022-02-28 23:48:31 -0300

    Change project description

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Item from '../components/Item';
-// import List from '../components/List';
 import Card from '../components/Card';
 import {
   Container,
